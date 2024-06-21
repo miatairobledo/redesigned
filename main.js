0ab06bce9f9808e62f6e598614ae29900874bb66
miatairobledo <lilpulgo211@gmail.com>
2024-06-21 17:57:56 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,7 +1,6 @@
 import fetch from 'node-fetch';
 import faker from 'faker';
 import http from 'http';
-import https from 'https';
 import { URL } from 'url';
 
 const { Headers } = fetch;
@@ -69,77 +68,79 @@ async function fetchAndApply(request) {
         const region = faker.address.countryCode();
         const geolocation = generateFakeGeolocation();
         const fake_time = generateFakeTime();
-        
-        let response = null;
-        let url = new URL(request.url);
+
+        let url = new URL(request.url, `http://${request.headers.host}`);
         let url_hostname = url.hostname;
-        
+
         if (httpsProtocol) {
             url.protocol = 'https:';
         } else {
             url.protocol = 'http:';
         }
-        
+
         const upstream_domain = await device_status(user_agent) ? upstream : upstream_mobile;
-        
+
         url.host = upstream_domain;
         url.pathname = upstream_path + url.pathname;
-        
+
         let method = request.method;
         let request_headers = new Headers(request.headers);
-        
+
         request_headers.set('Host', upstream_domain);
         request_headers.set('Referer', `${url.protocol}//${url_hostname}`);
         request_headers.set('cf-ipcountry', region);
         request_headers.set('cf-connecting-ip', ip_address);
-        
+        request_headers.set('User-Agent', user_agent);
+        request_headers.set('Geolocation', geolocation);
+        request_headers.set('Date', fake_time);
+
         request_headers.delete('Authorization');
         request_headers.delete('apikey');
         request_headers.delete('x-api-key');
-        
+
         let original_response = await fetch(url.href, {
             method: method,
             headers: request_headers,
-            body: request.body
+            body: request
         });
-        
+
         let connection_upgrade = request_headers.get("Upgrade");
         if (connection_upgrade && connection_upgrade.toLowerCase() === "websocket") {
             return original_response;
         }
-        
+
         let original_response_clone = original_response.clone();
         let original_text = null;
         let response_headers = original_response.headers;
         let new_response_headers = new Headers(response_headers);
         let status = original_response.status;
-        
+
         if (disable_cache) {
             new_response_headers.set('Cache-Control', 'no-store');
         }
-        
+
         new_response_headers.set('access-control-allow-origin', '*');
         new_response_headers.set('access-control-allow-credentials', 'true');
         new_response_headers.delete('content-security-policy');
         new_response_headers.delete('content-security-policy-report-only');
         new_response_headers.delete('clear-site-data');
-        
+
         if (new_response_headers.get("x-pjax-url")) {
             new_response_headers.set("x-pjax-url", response_headers.get("x-pjax-url").replace(`//${upstream_domain}`, `//${url_hostname}`));
         }
-        
+
         const content_type = new_response_headers.get('content-type');
         if (content_type != null && content_type.includes('text/html') && content_type.includes('UTF-8')) {
             original_text = await replace_response_text(original_response_clone, upstream_domain, url_hostname);
         } else {
             original_text = await original_response_clone.text();
         }
-        
+
         response = new Response(original_text, {
             status,
             headers: new_response_headers
         });
-        
+
         return response;
     } catch (err) {
         console.error('Error occurred:', err);
@@ -156,7 +157,7 @@ const server = http.createServer(async (req, res) => {
     };
 
     const proxyResponse = await fetchAndApply(request);
-    
+
     res.writeHead(proxyResponse.status, Object.fromEntries(proxyResponse.headers.entries()));
     res.end(await proxyResponse.text());
 });
